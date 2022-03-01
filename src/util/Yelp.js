28bd3eddaad50699b6e71a7e2c156a10ee164a7e
mgrzhdnv <mgrazhdanov@gmail.com>
2022-03-01 21:27:59 +0300

    compared SearchBar file

---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+const apiKey = 'WW8szNBTwD6kKxpbyFU79HjcwdJ20m9dLuZ1mtsfo3xnxPggxWOMi4aIJWXo0NyZufRoHon9t6Laf6NVbvG1FP6YPPaZZYE_e5AHByM1k-AHDMhDEbwCHP0mAEseYnYx';
+
+const yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            }
+        });
+    }
+};
+
+export default yelp;
